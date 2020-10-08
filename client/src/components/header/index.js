496bdccfc97757786ce611e05e1adf36dae0d7b5
Christian Fei <crifei93@gmail.com>
2020-10-08 17:16:18 +0200

    user

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -10,7 +10,7 @@ const Header = (props) => (
         <a class='navbar-brand' href='/'>Decent</a>
         <div class='navbar-nav'>
           {props.user && props.user.picture && <img src={props.user.picture} style='height: 2.5em; border-radius: 50%;' />}
-          <Link onClick={isLoggedIn() ? logout : login} class='nav-link' href='/'>{isLoggedIn() ? 'Logout' : 'Login'}</Link>
+          <button onClick={isLoggedIn() ? logout : login} class='nav-link' href='/'>{isLoggedIn() ? 'Logout' : 'Login'}</button>
         </div>
       </div>
     </nav>
--- client/src/routes/home/index.js
+++ client/src/routes/home/index.js
@@ -2,11 +2,10 @@ import { h } from 'preact'
 import { Link } from 'preact-router/match'
 
 const Home = (props) => {
-  // const { loginWithRedirect } = useAuth0()
   return (
     <div class='container py-5'>
       <h1 class='title mb-5'>Welcome to Decent</h1>
-      <p><a class='btn btn-primary btn-sm' href='/login'>Log In</a> and enjoy a decent Twitter reading experience</p>
+      {!props.user && <p><a class='btn btn-primary btn-sm' href='/login'>Log In</a> and enjoy a decent Twitter reading experience</p>}
       {props.user && (
         <p class='lead'>
           <pre>{JSON.stringify(props.user, null, 2)}</pre>
