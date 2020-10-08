890fd90b070e042cb8959ab6434b4a26a1034a84
Christian Fei <crifei93@gmail.com>
2020-10-08 18:56:42 +0200

    ui

--- client/src/routes/home/index.js
+++ client/src/routes/home/index.js
@@ -5,8 +5,11 @@ import { login } from '../../auth-service'
 const events = {
   handleSubmitSetup: (event) => {
     let twitterHandle = event.target.querySelector('#twitterHandle').value
+    let followingList = event.target.querySelector('#followingList').value
     twitterHandle = twitterHandle.replace('@', '')
+    followingList = followingList.replace(/@/gi, '').split(' ').filter(Boolean)
     console.log('twitterHandle', twitterHandle)
+    console.log('followingList', followingList)
     event.preventDefault()
   }
 }
@@ -18,13 +21,25 @@ const Home = (props) => {
       {props.user === null && <p><span onClick={login} class='btn btn-sm' href='/'>Login</span> and enjoy a decent Twitter reading experience</p>}
       {props.user && !props.user.setupComplete && (
         <form onSubmit={events.handleSubmitSetup}>
-          <div class='form-group'>
+          <div class='form-group mb-5'>
             <label for='twitterHandle' class='col-form-label'>Enter your Twitter handle if you want to retrieve the current users you follow</label>&nbsp;
             <div class=''>
               <input type='text' class='form-control' placeholder='@elonmusk' id='twitterHandle' aria-describedby='twitterHandleHelp' />
               <small id='twitterHandleHelp' class='form-text text-muted'>We'll never share your Twitter handle with anyone else.</small>
             </div>
           </div>
+          <div class='form-group mb-5'>
+            <p class='lead'>
+            If you prefer, enter below the users you want to follow
+            </p>
+            <textarea class='form-control' id='followingList' rows='3'>
+              @elonmusk
+              @lexfridman
+              @MKBHD
+            </textarea>
+            <small id='followingListHelp' class='form-text text-muted'>You can always change this later on, no worries.</small>
+          </div>
+          <button class='btn btn-primary'>Complete setup</button>
         </form>
       )}
     </div>
