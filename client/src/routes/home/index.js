3487077b46aa70170245be5fa304b7aadb364cf6
Christian Fei <crifei93@gmail.com>
2020-10-08 18:37:23 +0200

    handle twitter submit

--- client/src/routes/home/index.js
+++ client/src/routes/home/index.js
@@ -2,18 +2,29 @@ import { h } from 'preact'
 import { Link } from 'preact-router/match'
 import { login } from '../../auth-service'
 
+const events = {
+  handleSubmitSetup: (event) => {
+    let twitterHandle = event.target.querySelector('#twitterHandle').value
+    twitterHandle = twitterHandle.replace('@', '')
+    console.log('twitterHandle', twitterHandle)
+    event.preventDefault()
+  }
+}
+
 const Home = (props) => {
   return (
     <div class='container py-5'>
       <h1 class='title mb-5'>Welcome to Decent</h1>
       {props.user === null && <p><span onClick={login} class='btn btn-sm' href='/'>Login</span> and enjoy a decent Twitter reading experience</p>}
       {props.user && !props.user.setupComplete && (
-        <form class='form'>
+        <form onSubmit={events.handleSubmitSetup}>
           <div class='form-group'>
-            <label for='handle'>Enter your Twitter handle if you want to retrieve the current users you follow</label>&nbsp;
-            <input type='text' class='form-control' placeholder='@elonmusk' id='handle' aria-describedby='twitterHandleHelp' />
+            <label for='twitterHandle' class='col-form-label'>Enter your Twitter handle if you want to retrieve the current users you follow</label>&nbsp;
+            <div class=''>
+              <input type='text' class='form-control' placeholder='@elonmusk' id='twitterHandle' aria-describedby='twitterHandleHelp' />
+              <small id='twitterHandleHelp' class='form-text text-muted'>We'll never share your Twitter handle with anyone else.</small>
+            </div>
           </div>
-          <small id='twitterHandleHelp' class='form-text text-muted'>We'll never share your Twitter handle with anyone else.</small>
         </form>
       )}
     </div>
