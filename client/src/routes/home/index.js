1a7b55c717a75f18f60fdb9f3369dbfb74b6b435
Christian Fei <crifei93@gmail.com>
2020-10-08 17:19:39 +0200

    login

--- client/src/routes/home/index.js
+++ client/src/routes/home/index.js
@@ -1,11 +1,12 @@
 import { h } from 'preact'
 import { Link } from 'preact-router/match'
+import { login } from '../../auth-service'
 
 const Home = (props) => {
   return (
     <div class='container py-5'>
       <h1 class='title mb-5'>Welcome to Decent</h1>
-      {!props.user && <p><a class='btn btn-primary btn-sm' href='/login'>Log In</a> and enjoy a decent Twitter reading experience</p>}
+      {!props.user && <p><span onClick={login} class='nav-link' href='/'>Login</span> and enjoy a decent Twitter reading experience</p>}
       {props.user && (
         <p class='lead'>
           <pre>{JSON.stringify(props.user, null, 2)}</pre>
