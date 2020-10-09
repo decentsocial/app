45f6c5363244c68f9598d6e73002883df491c1d3
Christian Fei <crifei93@gmail.com>
2020-10-09 10:57:18 +0200

    settings link

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,4 +1,5 @@
 import { h } from 'preact'
+import { Link } from 'preact-router'
 import { login, logout } from '../auth-service'
 
 const Header = (props) => {
@@ -11,7 +12,7 @@ const Header = (props) => {
             <img class='' src='/assets/icons/favicon-32x32.png' alt='' style='height: 2rem; margin-right: 0.5em;' />
           </a>
           <div class='navbar-nav animate__animated animate__flash'>
-            {props.user && props.user.picture && <img src={props.user.picture} class='px-3' style='height: 2.5em; border-radius: 50%;' />}
+            {props.user && props.user.picture && <Link href='/settings'><img src={props.user.picture} class='px-3' style='height: 2.5em; border-radius: 50%;' /></Link>}
             <a href='#' onClick={props.user ? logout : login} class='nav-link btn btn-sm'>{props.user ? 'Logout' : 'Login'}</a>
           </div>
         </div>
