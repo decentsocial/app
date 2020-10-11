a4b5622333e7755701728fbd1d7d5365499f8dca
Christian Fei <crifei93@gmail.com>
2020-10-11 21:23:44 +0200

    no flex timeline

--- client/src/components/timeline.js
+++ client/src/components/timeline.js
@@ -28,7 +28,7 @@ const Timeline = (props) => {
         </div>
       </div>
       <div class='col-lg-6 col-md-12 p-0 mx-auto'>
-        <ul class='list-group1 border-0 d-flex flex-column m-0 p-0'>
+        <ul class='border-0 m-0 p-0'>
           <VirtualList
             width='100%'
             height='100vh'
@@ -36,17 +36,17 @@ const Timeline = (props) => {
             itemSize={(i => {
               const text = timeline[i].formatted
               const newLinesCount = text.split('\n').length + 1
-              return 100 + newLinesCount * 20 + text.length * 0.6
+              return 120 + newLinesCount * 20 + text.length * 0.6
             })}
             overscanCount={10}
             renderItem={({ index, style, t = timeline[index] }) =>
-              <li id={`t${+new Date(t.date)}`} tabIndex={index + 1} key={index} style={style} class={timelineStyles.tweet + ' d-flex flex-column align-content-stretch p-0 border-0 py-5'}>
-                <div class='d-flex w-100 justify-content-between'>
+              <li id={`t${+new Date(t.date)}`} tabIndex={index + 1} key={index} style={style} class={timelineStyles.tweet + ' p-0 border-0 py-5'}>
+                <div class=''>
+                  <small class='float-right text-muted has-tooltip'><a href={t.link} tabIndex={-1} target='_blank' rel='noopener noreferrer'>{new Date(t.date).toISOString().substring(11, 16)}</a></small>
                   <h5 class='mb-1 text-muted'>
                     <div style={`display: inline-block; border-radius: 50%; height: 2em; width: 2em; vertical-align: middle; background-size: contain; background-image: url(${t.authorAvatar})`} />
                     &nbsp;&nbsp;&nbsp;&nbsp;{t.author}
                   </h5>
-                  <small class='text-muted has-tooltip'><a href={t.link} tabIndex={-1} target='_blank' rel='noopener noreferrer'>{new Date(t.date).toISOString().substring(11, 16)}</a></small>
                   <span class='tooltip blue'><p>{t.date}</p></span>
                 </div>
                 <p class='w-100 mb-1 text-left py-2'>{t.formatted}</p>
