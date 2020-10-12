c81945f1e93a19dfc5a073c507eed3078efc6792
Christian Fei <crifei93@gmail.com>
2020-10-12 19:05:40 +0200

    Revert "tweet styles"
    This reverts commit b5cdef7164b9d88b8cc3612cc0c1d1f923b36ffc.

--- client/src/components/tweet.js
+++ client/src/components/tweet.js
@@ -3,17 +3,20 @@ import tweetStyles from './tweet.css'
 import Linkify from 'react-linkify'
 
 const Tweet = (props) => {
-  const tweet = props.tweet
+  const t = props.tweet
   return (
     <div class={tweetStyles.tweet + ' p-0 border-0 py-5'}>
-      <small class='float-right text-muted has-tooltip'><a href={tweet.link} tabIndex={-1} target='_blank' rel='noopener noreferrer'>{new Date(tweet.date).toISOString().substring(0, 16)}</a></small>
-      <h5 class='mb-1 text-muted text-left'>
-        <div style={`display: inline-block; border-radius: 50%; margin-right: 2em; height: 2em; width: 2em; vertical-align: middle; background-size: contain; background-image: url(${tweet.authorAvatar})`} />
-        {tweet.author}
-      </h5>
-      <span class='tooltip blue'><p>{tweet.date}</p></span>
-      <p class='lead mt-4 text-left py-2'><Linkify>{tweet.formatted || tweet.text}</Linkify></p>
+      <div class=''>
+        <small class='float-right text-muted has-tooltip'><a href={t.link} tabIndex={-1} target='_blank' rel='noopener noreferrer'>{new Date(t.date).toISOString().substring(0, 16)}</a></small>
+        <h5 class='mb-1 text-muted text-left'>
+          <div style={`display: inline-block; border-radius: 50%; margin-right: 2em; height: 2em; width: 2em; vertical-align: middle; background-size: contain; background-image: url(${t.authorAvatar})`} />
+          {t.author}
+        </h5>
+        <span class='tooltip blue'><p>{t.date}</p></span>
+      </div>
+      <p class='w-100 mb-1 text-left py-2'><Linkify>{t.formatted || t.text}</Linkify></p>
     </div>
+
   )
 }
 
