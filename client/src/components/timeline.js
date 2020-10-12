b45799de162f9f1f8f7395ecac12e131ba8c8564
Christian Fei <crifei93@gmail.com>
2020-10-12 19:05:52 +0200

    Revert "extract tweet component"
    This reverts commit f54bdd75d726070033bedaa55eba5026b3607cdd.

--- client/src/components/timeline.js
+++ client/src/components/timeline.js
@@ -1,7 +1,8 @@
 import { h } from 'preact'
+import { useState } from 'preact/hooks'
 import timelineStyles from './timeline.css'
-import Tweet from './tweet'
 import VirtualList from 'react-tiny-virtual-list'
+import Linkify from 'react-linkify'
 
 const Timeline = (props) => {
   const timeline = (props.timeline || [])
@@ -24,7 +25,17 @@ const Timeline = (props) => {
           scrollToAlignment='center'
           overscanCount={10}
           renderItem={({ index, style, t = timeline[index] }) =>
-            <Tweet id={`t${+new Date(t.date)}`} tweet={timeline[index]} tabIndex={index + 2} key={index} style={style} />}
+            <div id={`t${+new Date(t.date)}`} tabIndex={index + 2} key={index} style={style} class={timelineStyles.tweet + ' p-0 border-0 py-5'}>
+              <div class=''>
+                <small class='float-right text-muted has-tooltip'><a href={t.link} tabIndex={-1} target='_blank' rel='noopener noreferrer'>{new Date(t.date).toISOString().substring(0, 16)}</a></small>
+                <h5 class='mb-1 text-muted text-left'>
+                  <div style={`display: inline-block; border-radius: 50%; margin-right: 2em; height: 2em; width: 2em; vertical-align: middle; background-size: contain; background-image: url(${t.authorAvatar})`} />
+                  {t.author}
+                </h5>
+                <span class='tooltip blue'><p>{t.date}</p></span>
+              </div>
+              <p class='w-100 mb-1 text-left py-2'><Linkify>{t.formatted || t.text}</Linkify></p>
+            </div>}
         />
       </div>
     </div>
