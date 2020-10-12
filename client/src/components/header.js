30a33a1274619ef5e27a6637cbfd6a3e6d50367b
Christian Fei <crifei93@gmail.com>
2020-10-12 16:58:15 +0200

    hide show header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -11,12 +11,12 @@ class Header extends Component {
       <header>
         <nav class={'navbar navbar-white bg-white static-top fixed-top ' + `${styles.nav} ${closed ? styles.notVisible : styles.visible}`}>
           <div class='container'>
-            <a class='navbar-brand bg-white p-2 text-dark font-weight-bold' href='/' style='font-size: 2.1rem;'>
+            <a class='navbar-brand bg-white p-2 text-dark font-weight-bold' href='/' style='font-size: 2.1rem;' onClick={() => setClosed(closed => !closed)}>
               <img class='' src='/assets/icons/favicon-32x32.png' alt='' style='height: 2rem; margin-right: 0.5em;' />
             </a>
             <div class='bg-white p-2'>
-              {props.user && props.user.picture && <Link href='/settings'><img src={props.user.picture} class='px-3' style='height: 2.5em; border-radius: 50%;' /></Link>}
-              <a href='#' onClick={props.user ? logout : login} class='btn btn-sm'>{props.user ? 'Logout' : 'Login'}</a>
+              {props.user && props.user.picture && <Link href='/settings' onClick={() => setClosed(closed => !closed)}><img src={props.user.picture} class='px-3' style='height: 2.5em; border-radius: 50%;' /></Link>}
+              <a href='#' onClick={() => { props.user ? logout() : login() }} class='btn btn-sm'>{props.user ? 'Logout' : 'Login'}</a>
             </div>
           </div>
         </nav>
