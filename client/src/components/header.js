3ff958bc9d85d40f758accc78e30ea0cc4a666de
Christian Fei <crifei93@gmail.com>
2020-10-14 20:03:18 +0200

    bg-white btn

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -21,7 +21,7 @@ class Header extends Component {
             </div>
           </div>
         </nav>
-        <button tabIndex={0} style='z-index: 9999;' class={styles.toggle + ' btn btn-sm'} onClick={() => setClosed(closed => !closed)}>
+        <button tabIndex={0} style='z-index: 9999;' class={styles.toggle + ' btn btn-sm bg-white rounded'} onClick={() => setClosed(closed => !closed)}>
           {props.loading ? <svg width='24' height='24' viewBox='0 0 16 16' class={`bi bi-arrow-repeat ${styles.rotate}`} fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
             <path d='M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z' />
             <path fill-rule='evenodd' d='M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z' />
