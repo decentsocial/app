d645894d058b41d94e757b60347de1b84c33a9ef
Christian Fei <crifei93@gmail.com>
2020-10-14 15:25:46 +0200

    wip state with zustand

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -18,38 +18,16 @@ export default class App extends Component {
   constructor () {
     super()
     this.state = {
-      timeline: [],
-      alert: undefined,
-      search: undefined
+      search: undefined,
+      filteredTimeline: undefined
     }
   }
 
   componentDidMount () {
-    useStore.getState().getUserInfo()
-
-    let cachedTimeline = window.localStorage.getItem('timeline')
-    let since
-    if (cachedTimeline && cachedTimeline.length > 0) {
-      cachedTimeline = JSON.parse(cachedTimeline)
-      since = cachedTimeline.reduce((newest, curr) => newest < +new Date(curr.time) ? +new Date(curr.time) : newest, +new Date(cachedTimeline[0].date))
-      this.setState({ timeline: cachedTimeline })
-    }
-
-    this.setState({ alert: 'Loading updates..' })
-
-    ApiService.getUserTimeline({ since })
-      .then(newTimeline => {
-        const timeline = newTimeline
-        if (cachedTimeline && cachedTimeline.length > 0) timeline.push(...cachedTimeline)
-        this.setState({ timeline, alert: "You're up to date." })
-        window.localStorage.setItem('timeline', JSON.stringify(timeline))
-        cachedTimeline = timeline
-        setTimeout(() => { this.setState({ alert: undefined }) }, 1500)
-      })
-      .catch(err => {
-        console.error(err)
-        this.setState({ timeline: [], alert: undefined })
-      })
+    const state = useStore.getState()
+    state.getUserInfo()
+    state.loadCachedTimeline()
+    state.getUserTimeline()
   }
   keyMap = { 
     EXIT: 'esc',
@@ -66,12 +44,12 @@ export default class App extends Component {
     }
   }
 
-  performSearch (e) {
+  performSearch (e, timeline = []) {
     const search = e.target.value.toLowerCase()
     console.log('performSearch', search)
     trackEvent('performed-search')
     this.setState({
-      filteredTimeline: this.state.timeline
+      filteredTimeline: timeline
         .filter(t => t.author.toLowerCase().includes(search) || t.text.toLowerCase().includes(search))
     })
   }
@@ -80,15 +58,17 @@ export default class App extends Component {
     if (window.location.hash && window.location.hash.length > 1) return null
     trackEvent('rendered-timeline')
     const user = useStore(state => state.user)
+    const timeline = useStore(state => state.timeline)
+    const alert = useStore(state => state.alert)
     return (
       <div class=''>
         <Header user={user} />
         <Router>
-          <Home path='/' user={user} timeline={this.state.filteredTimeline || this.state.timeline} />
+          <Home path='/' user={user} timeline={this.state.filteredTimeline || timeline} />
           <Settings path='/settings' user={user} />
-          <Status path='/status/:id' timeline={this.state.timeline} />
+          <Status path='/status/:id' timeline={timeline} />
         </Router>
-        <Alert alert={this.state.alert} />
+        <Alert alert={alert} />
         <GlobalHotKeys keyMap={this.keyMap} handlers={this.handlers} />
         {this.state.search && 
           <div style='position: fixed; top: 0; left: 0; right: 0; background: white; padding: 3em 2em; box-shadow: 5px 5px 5px #ddd;'>
@@ -97,7 +77,7 @@ export default class App extends Component {
                 <div class="col-md-9">
                   <div class="input-group m-0">
                   <span class="input-group-text">Search</span>
-                    <input autofocus onInput={(e) => this.performSearch(e)} class="form-control" type="text" placeholder="Type in your search terms" />
+                    <input autofocus onInput={(e) => this.performSearch(e, timeline)} class="form-control" type="text" placeholder="Type in your search terms" />
                   </div>
                 </div>
                 <div class="col-md-3">
--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,8 @@ import * as ApiService from './api-service'
 
 import create from 'zustand'
 
-export default create(set => ({
+export default create((set, get) => ({
+  alert: undefined,
   user: undefined,
   async getUserInfo () {
     return ApiService.getUserInfo()
@@ -10,6 +11,34 @@ export default create(set => ({
       .catch(err => {
         window.debug && console.error(err.message)
         set({ user: null })
+      })
+  },
+  timeline: [],
+  since: undefined,
+  loadCachedTimeline () {
+    let cachedTimeline = window.localStorage.getItem('timeline')
+    if (cachedTimeline && cachedTimeline.length > 0) {
+      cachedTimeline = JSON.parse(cachedTimeline)
+      const since = cachedTimeline.reduce((newest, curr) => newest < +new Date(curr.time) ? +new Date(curr.time) : newest, +new Date(cachedTimeline[0].date))
+      set({ timeline: cachedTimeline, since })
+    }
+  },
+  async getUserTimeline () {
+    const since = get().since
+    const cachedTimeline = get().timeline
+    set({ alert: 'Loading updates..' })
+    ApiService.getUserTimeline({ since })
+      .then(newTimeline => {
+        const timeline = newTimeline
+        if (cachedTimeline && cachedTimeline.length > 0) timeline.push(...cachedTimeline)
+        const since = timeline.reduce((newest, curr) => newest < +new Date(curr.time) ? +new Date(curr.time) : newest, +new Date(timeline[0].date))
+        set({ timeline, since, alert: "You're up to date." })
+        window.localStorage.setItem('timeline', JSON.stringify(timeline))
+        setTimeout(() => { set({ alert: undefined }) }, 1500)
+      })
+      .catch(err => {
+        console.error(err)
+        set({ timeline: [], alert: undefined })
       })
   }
 }))
