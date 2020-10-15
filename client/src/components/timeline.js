8efb78878de4f3078dd423dd9f4a077099a35e1a
Christian Fei <crifei93@gmail.com>
2020-10-15 17:25:46 +0200

    resume position wip

--- client/src/components/timeline.js
+++ client/src/components/timeline.js
@@ -3,6 +3,13 @@ import { Link, route } from 'preact-router'
 import timelineStyles from './timeline.css'
 import { Virtuoso } from 'react-virtuoso'
 
+const debouncedSave = debounce(({ startIndex } = {}) => {
+  console.log('saving', startIndex)
+  window.localStorage.setItem('lastTweetIndex', startIndex)
+}, 1000)
+
+const lastTweetIndex = +window.localStorage.getItem('lastTweetIndex')
+
 const Timeline = (props) => {
   const timeline = (props.timeline || [])
     .filter(t => props.retweets ? true : !t.retweet)
@@ -13,6 +20,8 @@ const Timeline = (props) => {
       style={{ width: '100%', height: '100vh' }}
       totalCount={timeline.length}
       overscan={5}
+      rangeChanged={debouncedSave}
+      initialTopMostItemIndex={lastTweetIndex}
       item={index => {
         const t = timeline[index]
         return (
@@ -33,3 +42,19 @@ const Timeline = (props) => {
 }
 
 export default Timeline
+
+function debounce (func, wait) {
+  // https://davidwalsh.name/javascript-debounce-function
+  var timeout
+  return function () {
+    var context = this; var args = arguments
+    var later = function () {
+      timeout = null
+      func.apply(context, args)
+    }
+    var callNow = !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+};
