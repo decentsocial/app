3479df465cad237f3f19646ddbd4384ddf1b224e
Christian Fei <crifei93@gmail.com>
2020-10-16 15:46:36 +0200

    add wip scrollSeek item for timeline

--- client/src/components/timeline.js
+++ client/src/components/timeline.js
@@ -18,7 +18,29 @@ const Timeline = (props) => {
     <Virtuoso
       style={{ width: '100%', height: '100vh' }}
       totalCount={timeline.length}
-      overscan={5}
+      overscan={1000}
+      scrollSeek={{
+        enter: velocity => Math.abs(velocity) > 1000,
+        exit: velocity => {
+          const shouldExit = Math.abs(velocity) < 200
+          return shouldExit
+        },
+        change: (_velocity, { startIndex, endIndex }) => {},
+        // You can use index to randomize
+        // and make the placeholder list more organic.
+        placeholder: ({ height, index }) => (
+          <div class={timelineStyles.tweet + ' border-0 py-5 mx-auto'} style={{ height }}>
+            <div class=''>
+              <small class='float-right text-muted'>__:__AM</small>
+              <h5 class='mb-1 text-muted text-left'>
+                <div style='display: inline-block; border-radius: 50%; margin-right: 2em; height: 2em; width: 2em; vertical-align: middle; background-size: contain; background-color: lightgrey' />
+            @____
+              </h5>
+            </div>
+            <p class='w-100 mb-1 text-left py-3'>_________________________________________________<br />__________________________________________________________<br />________________<br />_____</p>
+          </div>
+        )
+      }}
       rangeChanged={debouncedSave}
       initialTopMostItemIndex={lastTweetIndex}
       item={index => {
