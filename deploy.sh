08cd58aaefe77a6579083ba878d141b62107dc41
Christian Fei <crifei93@gmail.com>
2020-10-19 16:01:36 +0200

    remove orphans

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh root@167.172.47.204 "cd /var/www/decent_social_app/; git checkout .; git pull; npm i; cd client && npm i && npm run build; docker-compose build --parallel; docker-compose stop; docker-compose up -d"
+ssh root@167.172.47.204 "cd /var/www/decent_social_app/; git checkout .; git pull; npm i; cd client && npm i && npm run build; docker-compose build --parallel; docker-compose stop; docker-compose up -d --remove-orphans"
