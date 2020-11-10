50d47d1227cde161a99c21f3ebd969694c24faaa
Christian Fei <crifei93@gmail.com>
2020-11-10 12:57:15 +0100

    fix

--- client/src/routes/pro/index.js
+++ client/src/routes/pro/index.js
@@ -9,8 +9,8 @@ const Pro = (props) => {
     <div class='container mt-5 py-5'>
       <h1 class='title'>Pro</h1>
       {props.user.pro
-        ? <p>You're already PRO! 💥</p>}
-        : <a class='btn btn-danger' onClick={state.stripeCheckout}>Become PRO for 2.99$ / month</a>
+        ? <p>You're already PRO! 💥</p>
+        : <a class='btn btn-danger' onClick={state.stripeCheckout}>Become PRO for 2.99$ / month</a>}
     </div>
   )
 }
